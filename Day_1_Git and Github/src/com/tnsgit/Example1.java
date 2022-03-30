c45a3a82e5f1e4d23eb20dbbbcdc6bd16ce25ad2
hp <hp@LAPTOP-44LCNLGG>
2022-03-30 21:50:16 +0530

    first commit

---
+++ Day_1_Git and Github/src/com/tnsgit/Example1.java
@@ -0,0 +1,12 @@
+package com.tnsgit;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Manish singh");
+		System.out.println("Nikitha singh");
+
+	}
+
+}
