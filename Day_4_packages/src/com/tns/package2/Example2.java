51640f27530408ec2ae0e11d5ef9f7ccea1ea83a
hp <hp@LAPTOP-44LCNLGG>
2022-03-31 19:23:38 +0530

    first commit

---
+++ Day_4_packages/src/com/tns/package2/Example2.java
@@ -0,0 +1,11 @@
+package com.tns.package2;
+import com.tnspackages.*;
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		Example1 e=new Example1();
+		e.display();
+		}
+
+}
