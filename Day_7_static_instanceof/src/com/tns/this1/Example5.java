f0d96ab346157769d30e297686d38cae6f361092
hp <hp@LAPTOP-44LCNLGG>
2022-04-06 16:30:19 +0530

    first commit

---
+++ Day_7_static_instanceof/src/com/tns/this1/Example5.java
@@ -0,0 +1,26 @@
+package com.tns.this1;
+
+public class Example5 
+{
+	int a;
+	int b;
+	Example5()
+	{
+		this(10,20);
+		System.out.println("Inside default constructor");
+		
+	}
+	Example5(int a,int b)
+	{
+		this.a = a;
+		this.b = b;
+		System.out.println("Inside parameterized constructor");
+	}
+
+	public static void main(String[] args) 
+	{
+	Example5 n=new Example5();	
+
+	}
+
+}
