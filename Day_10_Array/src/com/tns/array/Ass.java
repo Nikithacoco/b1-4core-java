1b32dff73d86da348598399743986c54511fed27
hp <hp@LAPTOP-44LCNLGG>
2022-04-10 20:00:15 +0530

    first commit

---
+++ Day_10_Array/src/com/tns/array/Ass.java
@@ -0,0 +1,21 @@
+package com.tns.array;
+
+public class Ass 
+{
+
+	public static void main(String[] args) {
+		
+	 class  Student1
+	 {
+			public int roll_no;
+			public String name;
+			Student1(int roll_no,String name)
+			{
+			this.roll_no=roll_no;
+			this.name=name;
+			}
+	 }
+	}
+}
+
+	
