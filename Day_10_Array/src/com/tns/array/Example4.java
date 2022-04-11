dbb9dcc2755bc0ad0ef6f26647151e0c065a579a
hp <hp@LAPTOP-44LCNLGG>
2022-04-11 22:06:27 +0530

    first commit

--- Day_10_Array/src/com/tns/array/Example4.java
+++ Day_10_Array/src/com/tns/array/Example4.java
@@ -7,19 +7,21 @@ public class Example4 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		int arr[]=new int[n];
+		int arr[]=new int[5];
+
 		for(int i=0;i<arr.length;i++)
 		{
 			arr[i]=s.nextInt();
 		}
-		for(int i=0;i<arr.length;i++);
+		for(int i=0;i<arr.length;i++)
 		{
 			System.out.println(arr[i]);
 		}
-		s.close();
+			s.close();
+		
 
 	}
-}
+	}
 
 
 	
