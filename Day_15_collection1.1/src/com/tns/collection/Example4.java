a916bfcf1abc2483b94f6a21889e962fbe9adbbf
hp <hp@LAPTOP-44LCNLGG>
2022-04-16 21:19:34 +0530

    first commit

---
+++ Day_15_collection1.1/src/com/tns/collection/Example4.java
@@ -0,0 +1,16 @@
+package com.tns.collection;
+
+import java.util.Vector;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		Vector v=new Vector();
+		v.add("Nikitha");
+		v.add(11);
+		System.out.println(v);
+		
+
+	}
+
+}
