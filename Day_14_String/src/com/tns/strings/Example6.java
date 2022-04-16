9ca2b5d32a720505aed405d1404caa05041bc0d8
hp <hp@LAPTOP-44LCNLGG>
2022-04-16 20:45:14 +0530

    first commit

---
+++ Day_14_String/src/com/tns/strings/Example6.java
@@ -0,0 +1,12 @@
+package com.tns.strings;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		 StringBuffer sb = new  StringBuffer("Good");
+		 sb.append("job");
+		 System.out.println(sb);
+
+	}
+
+}
---
+++ Day_14_String/src/com/tns/strings/Example7.java
@@ -0,0 +1,15 @@
+package com.tns.strings;
+
+import java.util.StringTokenizer;
+
+public class Example7
+{
+
+	public static void main(String[] args) 
+	{
+		StringTokenizer st = new StringTokenizer("Lets try this");
+		System.out.println(st.nextToken());
+
+	}
+
+}
